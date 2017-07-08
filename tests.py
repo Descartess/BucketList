278c6245cd70de9ac3bd94e47581be4d5f52c34e
NyondoPaul <nyondopaul23@gmail.com>
2017-07-08 15:48:33 +0300

    code refactor

--- tests.py
+++ tests.py
@@ -1,17 +1,7 @@
-""" Tests for flask application """
+""" run all application tests """
 import unittest
-from app.views import Main
 
-class BucketListTest(unittest.TestCase):
-    """ Tests for bucket list application """
-    def setUp(self):
-        """ Generate essential parameters required for Tests to run """
-        self.username = " Paul Nyondo"
-        self.password = " password "
-        self.main = Main()
-    def testsignup(self):
-        """ Tests the sign up method """
-        self.main.adduser(self.username, self.password)
-        self.assertIn(self.username, self.main.users)
+from tests import *
 
-unittest.main()
+if __name__ == "__main__":
+    unittest.main()
