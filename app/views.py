e35e42ccd0bc72dd7899428e7ae46500c974d88b
NyondoPaul <nyondopaul23@gmail.com>
2017-07-08 15:48:03 +0300

    code refactor

--- app/views.py
+++ app/views.py
@@ -3,6 +3,32 @@ from flask import render_template
 
 from app import APP
 
+class Main(object):
+    """ Main application class for the application"""
+    def __init__(self):
+        self.users = {}
+        self.currentuser = ''
+        self.authenticated = False
+
+    def adduser(self, username, password):
+        ''' Adds signed up user to global dict of users '''
+        if username in self.users:
+            return ' User already exists'
+        self.users[username] = password
+        self.currentuser = username
+
+    def loginuser(self, username, password):
+        ''' checks availabilty of user credentials and logins in the person '''
+        if self.users[username] == password:
+            self.currentuser = username
+            self.authenticated = True
+        else:
+            pass
+    def logout(self):
+        '''logs out users '''
+        self.currentuser = ''
+        self.authenticated = False
+
 @APP.route('/')
 def index():
     return render_template("login.html")
@@ -10,19 +36,4 @@ def index():
 @APP.route('/about')
 def home():
     return render_template("signup.html")
-
-class Users(object):
-    """ Users class that holds all user actions """
-    def signup(self, username, password):
-        """ Signs Up a user """
-        return username, password
-
-class Main(Users):
-    """ Main application class for the application"""
-    def __init__(self):
-        self.users = {}
-
-    def adduser(self, username, password):
-        ''' Adds signed up user to global dict of users '''
-        user = self.signup(username, password)
-        self.users[user[0]] = user[0]
+    
