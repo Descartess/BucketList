6e59ad8e4d8e1cc36c51f403bd255e597008c8ee
NyondoPaul <nyondopaul23@gmail.com>
2017-07-13 17:14:49 +0300

    Code refactor to PEP-8

--- tests/test_bucketlist.py
+++ tests/test_bucketlist.py
@@ -6,13 +6,13 @@ class Test_BucketList_Class(unittest.TestCase):
     """unit test methods for Test_BucketListItem_Class """
     def setUp(self):
         """Initialisation before each test """
-        self.blist = BucketList("Adventure",30)
+        self.blist = BucketList("Adventure", 30)
 
     def test_init(self):
         """ test the __init__ method"""
         self.assertEqual(self.blist.name, "Adventure")
         self.assertEqual(self.blist.completed_by, 30)
-        self.assertEqual([],self.blist.list_items)
+        self.assertEqual([], self.blist.list_items)
         self.assertFalse(self.blist.completed)
 
     def test_add_item(self):
